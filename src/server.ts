441c187c41d4da87273da762ef425d2ce83f6180
Marcos Oliveira <marcosoliveira.rd@gmail.com>
2022-09-18 23:54:35 -0400

    routes test with overnight/js and superTest feature

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
-import "./Util/module-alias";
+import "./util/module-alias";
 import { Server } from "@overnightjs/core";
-import express from "express";
+import express, { Application } from "express";
+import { ForecastController } from "./controllers/forecast";
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
+    this.setupControllers();
   }
 
   private setupExpress(): void {
     this.app.use(express.json());
+    this.setupControllers();
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
   }
 }
