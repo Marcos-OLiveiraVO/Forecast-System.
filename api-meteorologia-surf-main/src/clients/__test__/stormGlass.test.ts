9351ee3b127a4fe45675bcc31ecbd8bc6d4737e6
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-11-02 23:38:27 -0300

    feature: clients tests and services

---
+++ api-meteorologia-surf-main/src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_3Hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client ', () => {
+  it('should return the normalized forecast from the stormGlass service', async () => {
+    const lat = -123.355421;
+    const lng = 532.351245;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
