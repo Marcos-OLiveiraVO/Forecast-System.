9306419eb49b245d913ee72ea5317cbb0325da78
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-11-05 20:18:47 -0300

    feature: forecast normalized

--- api-meteorologia-surf-main/src/clients/__test__/stormGlass.test.ts
+++ api-meteorologia-surf-main/src/clients/__test__/stormGlass.test.ts
@@ -11,7 +11,9 @@ describe('StormGlass client ', () => {
     const lat = -123.355421;
     const lng = 532.351245;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
