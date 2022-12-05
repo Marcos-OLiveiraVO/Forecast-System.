96bb65fcd98d64e07a9ac7e07bc6fb0aa1b8bc0a
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2022-12-04 22:22:59 -0300

    feature: mongoose validation

--- api-meteorologia-surf-main/test/functional/users.test.ts
+++ api-meteorologia-surf-main/test/functional/users.test.ts
@@ -1,8 +1,8 @@
 import { User } from '@src/models/user';
 
 describe('Users test functional', () => {
+  beforeEach(async () => await User.deleteMany({}));
   describe('When create a new user', () => {
-    beforeAll(async () => await User.deleteMany({}));
     it('Should create new user with successfully', async () => {
       const newUser = {
         name: 'John doe',
@@ -14,6 +14,36 @@ describe('Users test functional', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+  });
+
+  it('Should return 422 when there is a validation error', async () => {
+    const newUser = {
+      email: 'john@mail.com',
+      password: '1234',
+    };
+    const response = await global.testRequest.post('/users').send(newUser);
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      code: 422,
+      error: 'User validation failed: name: Path `name` is required.',
+    });
+  });
+
+  it('should return 409 when email already exist', async () => {
+    const newUser = {
+      name: 'John Doe',
+      email: 'john@mail.com',
+      password: '1234',
+    };
+    await global.testRequest.post('/users').send(newUser);
+    const response = await global.testRequest.post('/users').send(newUser);
+
+    expect(response.status).toBe(409);
+    expect(response.body).toEqual({
+      code: 409,
+      error: 'User validation failed: email: already exists in the database.',
     });
   });
 });
